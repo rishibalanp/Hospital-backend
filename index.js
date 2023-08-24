39a0eed83cf53eebae1e0f93ced4e2bd9ff5ac23
Rishibalan <rishibalanp@gmail.com>
2023-08-24 11:21:37 +0530

    changes

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const http = require('http').Server(app);
+const mongoose = require('mongoose');
+// const port = 3001;
 const userRoutes = require("./routes/user");
 const acoountRoutes = require("./routes/doctor");
 
-app.use('/user', userRoutes);
-app.use('/doctor', acoountRoutes);
+
+const User = require('./models/userModels')
+
+// rishibalanp
+// QVYprzuAOV8ZWXtA
+
+mongoose.connect("mongodb+srv://rishibalanp:QVYprzuAOV8ZWXtA@hospital-backend.o7l71yi.mongodb.net/?retryWrites=true&w=majority")
 
 
-app.listen(port, () => {
-    console.log(`listening ${port}`);
+// async function insert() {
+//     await User.create({
+//         name: 'Rishi',
+//         email: 'rishibalanp@gmail.com'
+//     })
+// }
+// insert();
+
+http.listen(3000, function () {
+    console.log('server is running port 3000')
 });
 
-app.get('/', (req, res) => {
-    res.send("hello");
-});
+// app.use('/user', userRoutes);
+// app.use('/doctor', acoountRoutes);
 
-app.get('/getdata', (req, res) => {
-    res.json({
-        "statusCode": 200,
-        "statusMessage": "success"
-    })
-})
+
+// app.listen(port, () => {
+//     console.log(`listening ${port}`);
+// });
+
+// app.get('/', (req, res) => {
+//     res.send("hello");
+// });
+
+// app.get('/getdata', (req, res) => {
+//     res.json({
+//         "statusCode": 200,
+//         "statusMessage": "success"
+//     })
+// })
